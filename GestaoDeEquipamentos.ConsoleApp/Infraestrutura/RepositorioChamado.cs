508003ac9dcfb9f1e5674464c86f114bf33408a2
DrElucidator <dr.elucidator@gmail.com>
2026-04-13 11:49:04 -0300

    opção de exclusão de chamado adicionada

--- GestaoDeEquipamentos.ConsoleApp/Infraestrutura/RepositorioChamado.cs
+++ GestaoDeEquipamentos.ConsoleApp/Infraestrutura/RepositorioChamado.cs
@@ -23,6 +23,59 @@ public class RepositorioChamado
             }
         }
     }
+    public bool Editar(string idSelecionado, Chamado novoChamado)
+    {
+        Chamado? chamadoSelecionado = SelecionarPorId(idSelecionado);
+
+        if (chamadoSelecionado == null)
+            return false;
+
+        chamadoSelecionado.titulo = novoChamado.titulo;
+        chamadoSelecionado.descricao = novoChamado.descricao;
+        chamadoSelecionado.dataAbertura = novoChamado.dataAbertura;
+
+        return true;
+    }
+
+    public bool Excluir(string idSelecionado)
+    {
+        for (int i = 0; i < chamados.Length; i++)
+        {
+            Chamado? c = chamados[i];
+
+            if (c == null)
+                continue;
+
+            if (c.id == idSelecionado)
+            {
+                chamados[i] = null;
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    public Chamado? SelecionarPorId(string idSelecionado)
+    {
+        Chamado? chamadoSelecionado = null;
+
+        for (int i = 0; i < chamados.Length; i++)
+        {
+            Chamado? c = chamados[i];
+
+            if (c == null)
+                continue;
+
+            if (c.id == idSelecionado)
+            {
+                chamadoSelecionado = c;
+                break;
+            }
+        }
+
+        return chamadoSelecionado;
+    }
 
     public Chamado?[] SelecionarTodos()
     {
